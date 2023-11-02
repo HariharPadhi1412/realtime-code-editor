0b6e2d3e07b30efa41f2462bd5413be74de2d75a
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-11-02 17:51:37 +0530

    created the Toast msg succcessfully

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import toast from "react-hot-toast";
 
 const Home = () => {
+  const [roomId, setRoomID] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+
+    const id = uuidv4();
+    setRoomID(id);
+
+    toast.success("Created a new room");
+  };
+
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
         <img className="homePageLogo" src="/code-sync.png" alt="logo"></img>
         <h4 className="mainLabel"> Paste Invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input className="inputBox" type="text" placeholder="ROOM ID"></input>
+          <input
+            className="inputBox"
+            type="text"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomID(e.target.value)}
+            value={roomId}
+          ></input>
           <input
             className="inputBox"
             type="text"
             placeholder="USERNAME"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
           ></input>
           <button className="btn joinBtn">Join</button>
           <span className="createInfo">
             If you don't have an invite then &nbsp;
-            <a href="/" className="newRoom">
+            <a onClick={createNewRoom} href="/" className="newRoom">
               new room
             </a>
           </span>
