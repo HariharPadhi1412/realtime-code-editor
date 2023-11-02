629444317432fdca989e17084ee2359354112dbf
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-11-02 19:21:04 +0530

    created the side panel

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import Clients from "../components/Clients";
+import Editor from "../components//Editor";
 
 const EditorPage = () => {
-  return (
-    <div>EditorPage</div>
-  )
-}
+  const [clients, useClients] = useState([
+    {
+      socketid: 1,
+      username: "123",
+    },
+    {
+      socketid: 2,
+      username: "1234",
+    },
+  ]);
 
-export default EditorPage
+  return (
+    <div className="MainWrapper">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImg" src="/code-sync.png" alt="logo"></img>
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Clients key={client.socketid} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn">Copy ROOM ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+
+      <div className="editorWrap">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
